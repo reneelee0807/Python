0360aee2371b96b269efab6919e98b89d2ce6e12
reneelee0807 <30913287+reneelee0807@users.noreply.github.com>
2018-04-29 22:24:07 +1200

    final edit

--- validator.py
+++ validator.py
@@ -4,7 +4,8 @@ import re
 
 class Validator:
 
-    dic_condition = {'emp_id': '[A-Z][0-9]{3}','sales_salary': '[0-9]{2,3}'}
+    dic_condition = {'emp_id': '[A-Z][0-9]{3}','sales_salary': '[0-9]{2,3}',
+                     'age':'[0-9]{2}'}
 
     def validate_item(self, item, regex):
         validate = re.compile(self.dic_condition[regex])
@@ -12,11 +13,6 @@ class Validator:
             return True
         else:
             return False
-
-
-    def validate_age(self, age):
-        condition = '[0-9]{2}'
-        return self.validate_item(condition, age)
 
     @staticmethod
     def validate_data(values, item):
@@ -44,12 +40,12 @@ class Validator:
 
     def validate_all(self, employee):
         if len(employee) >= 7:
-            items_valid = [self.validate(str(employee[0]), 'emp_id'),
+            items_valid = [self.validate_item(str(employee[0]), 'emp_id'),
                            self.validate_gender(str(employee[1])),
-                           self.validate_age(str(employee[2])),
-                           self.validate_sales_salary(str(employee[3]), 'sales_salary'),
+                           self.validate_item(str(employee[2]),'age'),
+                           self.validate_item(str(employee[3]), 'sales_salary'),
                            self.validate_bmi(str(employee[4])),
-                           self.validate_sales_salary(str(employee[5]), 'sales_salary'),
+                           self.validate_item(str(employee[5]), 'sales_salary'),
                            self.validate_date(str(employee[6]))]
         else:
             items_valid = {False, False, False, False, False, False, False}
