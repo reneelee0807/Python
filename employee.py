f6758c13d8ce0d43166d65969fd6fb15a9b87691
reneelee0807 <30913287+reneelee0807@users.noreply.github.com>
2018-04-29 21:08:26 +1200

    replace salary and birthday attribute

--- employee.py
+++ employee.py
@@ -3,12 +3,11 @@ class Employee:
 
     dictionary = {'_emp_id': 'emp_id', '_gender':'gender',
                   '_age': 'age', '_sales': 'sales',
-                  '_bmi': 'bmi'}
+                  '_bmi': 'bmi', '_salary': 'salary',
+                  '_birthday': 'birthday'}
 
-    def __init__(self, dictionary, salary, birthday):
+    def __init__(self, dictionary):
         self.__dict__.update(dictionary)
-        self._salary = salary
-        self._birthday = birthday
 
     def get_all_data(self):
         employee_info = [self._emp_id, self._gender, self._age, self._sales, self._bmi, self._salary, self._birthday]
